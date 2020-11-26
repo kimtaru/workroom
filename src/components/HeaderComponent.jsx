620124352a03b622ddeb6cd7b4c6170bf55a59b6
jinsol <kimtaru2@gmail.com>
2020-11-26 22:19:28 +0900

    주석 달았음

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import { Layout } from 'antd';
 import { Link } from 'react-router-dom';
-
 import $ from 'jquery';
 import jQuery from 'jquery';
 window.$ = window.jQuery = jQuery;
@@ -13,8 +12,9 @@ export default function HeaderComponent({ current }) {
     const arr = $('.menu').get();
     arr.forEach((element) => {
       $(element).removeClass('current');
-    });
+    }); // header menu item 전체의 클래스 제거
     $(arr[current]).addClass('current');
+    // 현재 선택된 메뉴에만 클래스 추가
   }, [current]);
 
   return (
