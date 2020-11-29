c3db3217c9fd3e8f0463810725d3bfc95a679d8c
jinsol <kimtaru2@gmail.com>
2020-11-29 17:50:38 +0900

    회원가입 validation 구현

---
+++ src/common-function/Common.js
@@ -0,0 +1,30 @@
+export function checkEmail(email) {
+  const regExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+  if (regExp.test(email)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+export function checkPwd(pwd) {
+  const num = pwd.search(/[0-9]/g);
+  const eng = pwd.search(/[a-z]/gi);
+  if (pwd.length < 8) {
+    return false; //8자리 이상인지
+  } else if (pwd.search(/\s/) !== -1) {
+    return false; //공백이 있는지
+  } else if (num < 0 || eng < 0) {
+    return false; //영문, 숫자 혼합되었는지
+  } else {
+    return true;
+  }
+}
+
+export function doubleCheck(email) {
+  if (email === 'kkk@kk.js') {
+    return true;
+  } else {
+    return false;
+  }
+}
--- src/components/UserJoinComponent.jsx
+++ src/components/UserJoinComponent.jsx
@@ -1,10 +1,74 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Checkbox } from 'antd';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+import { useState } from 'react';
+import * as common from '../common-function/Common';
 
 export default function UserJoinComponent() {
   const google = <FontAwesomeIcon icon={faGoogle} size="lg" />;
+
+  const email = useRef();
+  const pwd = useRef();
+  const pwdCheck = useRef();
+
+  const [emailMsg, setEmailMsg] = useState();
+  const [pwdMsg, setPwdMsg] = useState();
+  const [pwdCheckMsg, setPwdCheckMsg] = useState();
+  const [termOne, setTermOne] = useState(false);
+  const [termTwo, setTermTwo] = useState(false);
+
+  // 이메일 유효성 검증
+  const emailValidation = () => {
+    let _email = email.current.value;
+    if (common.checkEmail(_email)) {
+      if (common.doubleCheck(_email)) {
+        // 계정 중복체크 함수 만들어야 함 (중복이면 true, 아니면 false 반환 하도록)
+        setEmailMsg(2); // 이미 사용중인 계정입니다.
+      } else {
+        setEmailMsg(true);
+      }
+    } else {
+      setEmailMsg(3); // 올바른 이메일 형식이 아닙니다.
+    }
+  };
+  // 패스워드 유효성 검증
+  const pwdValidation = () => {
+    let _pwd = pwd.current.value;
+    if (common.checkPwd(_pwd)) {
+      setPwdMsg(true);
+    } else {
+      setPwdMsg(1);
+    }
+  };
+  //
+  const pwdCheckValidation = () => {
+    let _pwd = pwd.current.value;
+    let _pwdCheck = pwdCheck.current.value;
+    if (_pwd === _pwdCheck) {
+      setPwdCheckMsg(true);
+    } else {
+      setPwdCheckMsg(1);
+    }
+  };
+
+  const checkAll = (e) => {
+    setTermOne(e.target.checked);
+    setTermTwo(e.target.checked);
+  };
+
+  const termOneCheck = (e) => {
+    setTermOne(e.target.checked);
+  };
+  const termTwoCheck = (e) => {
+    setTermTwo(e.target.checked);
+  };
+
+  const userJoin = () => {
+    if (emailMsg && pwdMsg && pwdCheckMsg && termOne) {
+      console.log('계정 등록 완료!');
+    }
+  };
   return (
     <div id="UserJoinComponent">
       <div className="title">
@@ -14,22 +78,53 @@ export default function UserJoinComponent() {
       </div>
       <div className="form">
         <div className="label">이메일</div>
-        <input type="text" placeholder="이메일" />
+        <input
+          type="text"
+          placeholder="이메일"
+          ref={email}
+          onChange={emailValidation}
+        />
+        <div className="mssg">
+          {emailMsg === 2 && `이미 사용중인 계정입니다.`}
+          {emailMsg === 3 && `올바른 이메일 형식이 아닙니다.`}
+        </div>
         <div className="label">비밀번호</div>
-        <input type="password" placeholder="비밀번호" />
+        <input
+          type="password"
+          placeholder="비밀번호"
+          ref={pwd}
+          onChange={pwdValidation}
+        />
+        <div className="mssg">
+          {pwdMsg === 1 && `영문, 숫자 혼합하여 8자리 이상 20자리 이하`}
+        </div>
         <div className="label">비밀번호 확인</div>
-        <input type="password" placeholder="비밀번호 확인" />
-        <div className="term-div">
-          <Checkbox className="term">모두 동의합니다.</Checkbox>
+        <input
+          type="password"
+          placeholder="비밀번호 확인"
+          ref={pwdCheck}
+          onChange={pwdCheckValidation}
+        />
+        <div className="mssg">
+          {pwdCheckMsg === 1 && `비밀번호가 일치하지 않습니다.`}
+        </div>
+        <div className="first term-div">
+          <Checkbox className="term" onChange={checkAll}>
+            모두 동의합니다.
+          </Checkbox>
         </div>
         <div className="term-div">
-          <Checkbox className="term">개인정보 약관 동의(필수)</Checkbox>
+          <Checkbox className="term" checked={termOne} onChange={termOneCheck}>
+            개인정보 약관 동의(필수)
+          </Checkbox>
         </div>
         <div className="term-div">
-          <Checkbox className="term">제 3자 제공 동의(선택)</Checkbox>
+          <Checkbox className="term" checked={termTwo} onChange={termTwoCheck}>
+            제 3자 제공 동의(선택)
+          </Checkbox>
         </div>
         <div className="join-btn">
-          <button>계정 만들기</button>
+          <button onClick={userJoin}>계정 만들기</button>
         </div>
         <hr />
         <div className="googleLogin">{google} Google 계정으로 로그인</div>
